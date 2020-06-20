cfe502c1af6cdd99dcd641a76c09136a0bd20407
cownectar <josephparent613@gmail.com>
2020-06-20 20:01:13 +1000

    for-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
@@ -7,4 +9,31 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+        flash.notice = "Article '#{@article.title}' Created!"
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def destroy
+        @article = Article.find(params[:id]).destroy
+        redirect_to articles_path(@article)
+        flash.notice = "Article '#{@article.title}' Deleted!"
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+        redirect_to article_path(@article)
+    end
 end
